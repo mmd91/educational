ecdf77237d1f28e3437e4f6dba878f17bdb97009
mmd91 <malets.maria1@gmail.com>
2023-07-03 20:19:10 +0300

    data-table

--- sf/force-app/main/default/lwc/recordLayout/recordLayout.js
+++ sf/force-app/main/default/lwc/recordLayout/recordLayout.js
@@ -1,6 +1,65 @@
-import { api, LightningElement } from 'lwc';
+import { api, LightningElement, wire } from 'lwc';
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+
+const columns = [
+    { label: 'Label', fieldName: 'name' },
+    { label: 'Value', fieldName: 'value' },
+    
+];
+
 
 export default class RecordLayout extends LightningElement {
     @api recordId;
     @api objectApiName;
+    @api flexipageRegionWidth;
+
+    objectInfo = {};
+    objectInfoFields = [];
+    objectFieldNames = [];
+    
+    col = columns;
+   
+
+    @wire(getObjectInfo, { objectApiName: '$objectApiName'})
+    proccesObjectInfo(wiredData) {
+        if (wiredData.data){
+            this.objectInfo=wiredData.data;
+            console.log(this.objectInfo);
+            this.objectInfoFields = Object.values(this.objectInfo.fields);          
+            this.objectFieldNames = this.objectInfoFields.map(field=>this.objectApiName +'.'+ field.apiName);
+            console.log(JSON.stringify(this.objectFieldNames));
+        }
+    }
+
+    recordData;
+    @wire(getRecord, { recordId: '$recordId',fields:[], optionalFields:'$objectFieldNames' })
+    proccesRecordData(wiredData) {
+        if (wiredData.data){           
+            this.recordData = wiredData.data;
+            console.log('recordData');
+            console.log(JSON.stringify(this.recordData));
+            this.processData();
+        }
+
+        if (wiredData.error){
+            console.log('error:');
+            console.log(wiredData.error);
+        }
+    }
+    
+    data = [];
+    processData(){
+        let filedsWithValues = [];
+        this.objectFieldNames.forEach(fieldName=>{
+            let fieldValue = getFieldValue(this.recordData,fieldName);
+            filedsWithValues.push({apiName:fieldName, value:fieldValue });
+        });
+
+        this.data = filedsWithValues;
+    }
+
+
+  
+   
 }
